3e24acd457e12c9b9ff9f189db9d35801de42f0b
aaa <cnj991279@gmail.com>
2023-05-03 10:20:41 +0900

    [feat] 회원 CRUD

--- server/src/main/java/com/mainproject/seb43_main_023/Seb43Main023Application.java
+++ server/src/main/java/com/mainproject/seb43_main_023/Seb43Main023Application.java
@@ -2,7 +2,9 @@ package com.mainproject.seb43_main_023;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Seb43Main023Application {
 
---
+++ server/src/main/java/com/mainproject/seb43_main_023/member/service/MemberService.java
@@ -0,0 +1,54 @@
+package com.mainproject.seb43_main_023.member.service;
+
+import com.mainproject.seb43_main_023.exception.BusinessLogicException;
+import com.mainproject.seb43_main_023.exception.ExceptionCode;
+import com.mainproject.seb43_main_023.member.entity.Member;
+import com.mainproject.seb43_main_023.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public Member createMember(Member member) {
+        verifyExistsEmail(member.getEmail());
+
+
+        return memberRepository.save(member);
+    }
+
+    public Member updateMember(Member member) {
+        Member findMember = findVerifiedMember(member.getMemberId());
+        Optional.ofNullable(member.getNickname())
+                .ifPresent(nickname -> findMember.setNickname(nickname));
+        Optional.ofNullable(member.getMbti())
+                .ifPresent(mbti -> findMember.setMbti(mbti));
+
+        return memberRepository.save(member);
+    }
+
+    public void deleteMember(long memberId) {
+        Member verifiedMember = findVerifiedMember(memberId);
+        verifiedMember.setMemberStatus(Member.MemberStatus.MEMBER_QUIT);
+        memberRepository.save(verifiedMember);
+    }
+
+    public Member findVerifiedMember(long memberId) {
+        Optional<Member> optionalMember = memberRepository.findById(memberId);
+        Member findMember = optionalMember.orElseThrow(() ->
+                new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND));
+
+        return findMember;
+    }
+
+    private void verifyExistsEmail(String email) {
+        Optional<Member> member = memberRepository.findByEmail(email);
+        if (member.isPresent()) {
+            throw new BusinessLogicException(ExceptionCode.MEMBER_EXISTS);
+        }
+    }
+}
