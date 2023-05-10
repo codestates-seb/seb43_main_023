45637b94e38f71f049e62938cecedb03a69835d8
JoDaeUk <du39574@gmail.com>
2023-05-10 14:03:01 +0900

    [feat] mbti 여행지 데이터 작성

--- server/src/main/java/com/mainproject/seb43_main_023/exception/ExceptionCode.java
+++ server/src/main/java/com/mainproject/seb43_main_023/exception/ExceptionCode.java
@@ -7,6 +7,7 @@ public enum ExceptionCode {
     MEMBER_NOT_FOUND(404, "Member not found"),
     MEMBER_EXISTS(409, "Member exists"),
     POST_NOT_FOUND(HttpStatus.NOT_FOUND,"게시물을 찾을 수 없습니다."),
+    MBTI_NOT_FOUND(HttpStatus.NOT_FOUND,"잘못된 MBTI 입니다."),
     NO_PERMISSION(HttpStatus.FORBIDDEN, "권한이 없습니다.");
 
     @Getter
