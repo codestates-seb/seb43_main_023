b7a0567aeac88da6f93aa56305d2fada0e78ee7f
raccoon0814 <yjh44242168@gmail.com>
2023-05-12 01:23:15 +0900

    [Style] : 병합 전 push

--- client/src/Pages/RegionRecommend.tsx
+++ client/src/Pages/RegionRecommend.tsx
@@ -48,8 +48,8 @@ const RegionRecItemContainer = styled.div`
 `;
 
 const RegionRecImg = styled.div<SlideItemProps>`
-	width: 270px;
-	height: 270px;
+	width: 260px;
+	height: 260px;
 	background: ${(props) => (props.image ? `url(${props.image})` : `url('')`)}
 		center / cover no-repeat;
 	border-radius: 15px;
@@ -65,6 +65,37 @@ const RegionRecImg = styled.div<SlideItemProps>`
 `;
 
 function RegionRec() {
+	const regionImg = {
+		seoul:
+			'https://a.cdn-hotels.com/gdcs/production117/d150/1049d859-3926-4a0d-8ae2-d7e227f902c2.jpg?impolicy=fcrop&w=800&h=533&q=medium',
+		jeju: 'https://images.unsplash.com/photo-1579169825453-8d4b4653cc2c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+		gangwon:
+			'https://images.unsplash.com/photo-1676705910596-10a68dccbe8e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80',
+		gyeonggi:
+			'https://images.unsplash.com/photo-1607165398235-5f43c715f57b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1718&q=80',
+		busan:
+			'https://images.unsplash.com/photo-1638591751482-1a7d27fcea15?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742&q=80',
+		ulsan:
+			'https://images.unsplash.com/photo-1654061394903-fd141e2c97bb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+		daegu:
+			'https://images.unsplash.com/photo-1549898395-045c8fa2883b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+		daejeon:
+			'http://newsimg.hankookilbo.com/2016/10/21/201610211159329181_1.jpg',
+		incheon:
+			'https://images.unsplash.com/photo-1634131431002-8fe857eb64a6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80',
+		gwangju:
+			'https://images.unsplash.com/photo-1593419522318-81b7c346a3e8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2854&q=80',
+		gyeongnam:
+			'https://images.unsplash.com/photo-1529528070131-eda9f3e90919?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+		gyeongbuk:
+			'https://images.unsplash.com/photo-1669764372822-3cb8476d4f47?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80',
+		jeonnam: 'https://cdn.gjdream.com/news/photo/202101/605746_205437_2251.jpg',
+		jeonbuk:
+			'https://images.unsplash.com/photo-1653329315898-afe70b1335ed?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+		chungnam: 'https://cdn.imweb.me/thumbnail/20190505/5cce19e7b096e.jpg',
+		chungbuk: 'http://noms.templestay.com/images/TiImage/H/L/9569.png',
+	};
+
 	return (
 		<RegionRecContainer>
 			<RegionRecImage>
@@ -72,53 +103,53 @@ function RegionRec() {
 			</RegionRecImage>
 			<RegionRecItemContainer>
 				<Link to="/regiondetail">
-					<RegionRecImg image="https://a.cdn-hotels.com/gdcs/production117/d150/1049d859-3926-4a0d-8ae2-d7e227f902c2.jpg?impolicy=fcrop&w=800&h=533&q=medium">
+					<RegionRecImg image={regionImg.seoul}>
 						<div>서울</div>
 					</RegionRecImg>
 				</Link>
-				<RegionRecImg image="https://images.unsplash.com/photo-1579169825453-8d4b4653cc2c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80">
+				<RegionRecImg image={regionImg.jeju}>
 					<div>제주</div>
 				</RegionRecImg>
-				<RegionRecImg image="https://images.unsplash.com/photo-1676705910596-10a68dccbe8e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80">
+				<RegionRecImg image={regionImg.gangwon}>
 					<div>강원</div>
 				</RegionRecImg>
-				<RegionRecImg image="https://images.unsplash.com/photo-1607165398235-5f43c715f57b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1718&q=80">
+				<RegionRecImg image={regionImg.gyeonggi}>
 					<div>경기</div>
 				</RegionRecImg>
-				<RegionRecImg image="https://images.unsplash.com/photo-1638591751482-1a7d27fcea15?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742&q=80">
+				<RegionRecImg image={regionImg.busan}>
 					<div>부산</div>
 				</RegionRecImg>
-				<RegionRecImg image="https://images.unsplash.com/photo-1654061394903-fd141e2c97bb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80">
+				<RegionRecImg image={regionImg.ulsan}>
 					<div>울산</div>
 				</RegionRecImg>
-				<RegionRecImg image="https://images.unsplash.com/photo-1549898395-045c8fa2883b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80">
+				<RegionRecImg image={regionImg.daegu}>
 					<div>대구</div>
 				</RegionRecImg>
-				<RegionRecImg image="http://newsimg.hankookilbo.com/2016/10/21/201610211159329181_1.jpg">
+				<RegionRecImg image={regionImg.daejeon}>
 					<div>대전</div>
 				</RegionRecImg>
-				<RegionRecImg image="https://images.unsplash.com/photo-1634131431002-8fe857eb64a6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80">
+				<RegionRecImg image={regionImg.incheon}>
 					<div>인천</div>
 				</RegionRecImg>
-				<RegionRecImg image="https://images.unsplash.com/photo-1593419522318-81b7c346a3e8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2854&q=80">
+				<RegionRecImg image={regionImg.gwangju}>
 					<div>광주</div>
 				</RegionRecImg>
-				<RegionRecImg image="https://images.unsplash.com/photo-1529528070131-eda9f3e90919?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80">
+				<RegionRecImg image={regionImg.gyeongnam}>
 					<div>경상남도</div>
 				</RegionRecImg>
-				<RegionRecImg image="https://images.unsplash.com/photo-1669764372822-3cb8476d4f47?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80">
+				<RegionRecImg image={regionImg.gyeongbuk}>
 					<div>경상북도</div>
 				</RegionRecImg>
-				<RegionRecImg image="https://cdn.gjdream.com/news/photo/202101/605746_205437_2251.jpg">
+				<RegionRecImg image={regionImg.jeonnam}>
 					<div>전라남도</div>
 				</RegionRecImg>
-				<RegionRecImg image="https://images.unsplash.com/photo-1653329315898-afe70b1335ed?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80">
+				<RegionRecImg image={regionImg.jeonbuk}>
 					<div>전라북도</div>
 				</RegionRecImg>
-				<RegionRecImg image="https://cdn.imweb.me/thumbnail/20190505/5cce19e7b096e.jpg">
+				<RegionRecImg image={regionImg.chungnam}>
 					<div>충청남도</div>
 				</RegionRecImg>
-				<RegionRecImg image="http://noms.templestay.com/images/TiImage/H/L/9569.png">
+				<RegionRecImg image={regionImg.chungbuk}>
 					<div>충청북도</div>
 				</RegionRecImg>
 			</RegionRecItemContainer>
